a79ad79ae90fb60d6ce6645f70ebec87b08dd3fa
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-06-08 23:57:00 -0700

    challenge: DOM reload button and async/await syntax

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ console.log('Hello world');
 
 const URL = 'https://api.thedogapi.com/v1/images/search';
 
-fetch(URL)
+/* fetch(URL)
     .then(res => res.json())
     .then(data => {
         const img = document.querySelector('img');
         img.src = data[0].url;
-    });
+    }); */
+
+fetchData(URL);
 
 // Challenge: Create async/await syntax
+async function fetchData(ApiURL) {
+    const response = await fetch(ApiURL);
+    const data = await response.json();
+    let img = document.querySelector('img');
+    img.src = data[0].url;
+}
+
+function reload() {
+    window.location.reload();
+}
