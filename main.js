8ece73203515a6443d495590d641e22440102717
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-06-09 15:08:52 -0600

    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('Hello world');
 
-const URL = 'https://api.thedogapi.com/v1/images/search';
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=09';
 
 /* fetch(URL)
     .then(res => res.json())
@@ -9,7 +9,7 @@ const URL = 'https://api.thedogapi.com/v1/images/search';
         img.src = data[0].url;
     }); */
 
-fetchData(URL);
+/* fetchData(URL);
 
 // Challenge: Create async/await syntax
 async function fetchData(ApiURL) {
@@ -21,4 +21,30 @@ async function fetchData(ApiURL) {
 
 function reload() {
     window.location.reload();
+} */
+
+
+
+dogGrid(getData());
+
+function reload() {
+    window.location.reload();
+}
+
+async function getData() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    return data;
+}
+
+async function dogGrid(collection) {
+    const doggosSection = document.getElementById('dog-grid');
+    const data = await collection;
+    
+    Object.entries(data).forEach(thumbnail => {
+        let img = document.createElement('img');
+        img.src = thumbnail[1].url;
+        doggosSection.appendChild(img);
+    });
 }
