a07461f4d99e6483df1bf002ee4054ed200cd6cf
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-06-17 16:45:19 -0700

    Delete fav dogs function working

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+
+//TODO: Separate limit query from url's
 const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=16';
 const API_URL_FAVOURITES = 'https://api.thedogapi.com/v1/favourites?limit=16';
 const API_KEY = '&api_key=live_Zw2RVsJsCsJVGIm1mI08GbGWnZfS6GQ1LI10VoDslKtUWjmae0uBM6cON3Iy5jG0';
+const API_URL_FAVOURITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=live_Zw2RVsJsCsJVGIm1mI08GbGWnZfS6GQ1LI10VoDslKtUWjmae0uBM6cON3Iy5jG0`;
 const randomDoggos = document.getElementById('random-doggos');
 const favouriteDoggos = document.getElementById('favourite-doggos');
 
 
-loadDogRandomGrid(API_URL_RANDOM, API_KEY, randomDoggos);
-// loadDogFavsGrid(API_URL_FAVOURITES, API_KEY);
+//loadDogRandomGrid(API_URL_RANDOM, API_KEY, randomDoggos);
+loadDogFavsGrid(API_URL_FAVOURITES, API_KEY);
 
 
 function reload() {
@@ -27,13 +30,17 @@ function createCard(urlDoggo, sectionID, pinMode, cardID) {
     imgDoggo.setAttribute('class', 'doggo');
     imgDoggo.setAttribute('alt', 'doggo-picture');
     pinSave.setAttribute('class', 'pin-save');
-    pinSave.onclick = () => saveFavDogs(cardID);
 
     imgDoggo.src = urlDoggo;
-    //Select pin icon
-    pinMode === 'unsave'
-        ? pinSave.innerHTML = '<img src=\"/assets/pin-unsave.svg\" alt="pin icon for saving">'
-        : pinSave.innerHTML = '<img src=\"/assets/pin-save.svg\" alt="pin icon for saving">'
+
+    //Select pin icon and behaviour
+    if (pinMode === 'unsave') {
+        pinSave.innerHTML = '<img src=\"/assets/pin-unsave.svg\" alt="pin icon for saving">';
+        pinSave.onclick = () => deleteFavDogs(cardID);
+    } else {
+        pinSave.innerHTML = '<img src=\"/assets/pin-save.svg\" alt="pin icon for saving">';
+        pinSave.onclick = () => saveFavDogs(cardID);
+    }
     
     title.innerText = 'Description of this turtle';
     description.innerText = 'This kind of turtle is epic and there are just a few of these living in kings landing and Narnia.';
@@ -77,7 +84,7 @@ async function loadDogRandomGrid(ApiURL, ApiKey, sectionID) {
 
     Object.entries(data).forEach(thumbnail => {
         const [imageURL, imageID] = [thumbnail[1].url, thumbnail[1].id];
-
+        console.log(thumbnail);
         createCard(imageURL, sectionID,'save', imageID);
     });
 }
@@ -90,8 +97,9 @@ async function loadDogFavsGrid(ApiURL, ApiKey) {
     }
 
     Object.entries(data).forEach(thumbnail => {
-        console.log(thumbnail)
-        createCard(thumbnail[1].image.url, favouriteDoggos,'unsave');
+        console.log(thumbnail);
+        const [imageURL, imageID] = [thumbnail[1].image.url, thumbnail[1].id];
+        createCard(imageURL, favouriteDoggos,'unsave', imageID);
     });
 }
 
@@ -113,20 +121,23 @@ async function saveFavDogs(id) {
     if (res.status !== 200) {
         const data = await res.json();
         console.log('There was an error: ' + data.message); 
+    } else {
+        console.log('Dog saved');
     }
 }
 
-
-//TODO:
 async function deleteFavDogs(id) {
-    const API = API_URL_FAVOURITES + API_KEY;
+    const API = API_URL_FAVOURITES_DELETE(id);
     const res = await fetch(API, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            image_id: id
-        }),
-    })
+        method: 'DELETE'
+    });
+
+    console.log('Unsave');
+
+    const data = await res.json();
+    if (res.status !== 200) {
+        console.log('There was an error: ' + data.message); 
+    } else {
+        console.log('Dog unsaved');
+    }
 };
