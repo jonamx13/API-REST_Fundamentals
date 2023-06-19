3bada5f17b3acc9922191383f0b49ee943d02eba
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-06-19 15:07:17 -0600

    Title done, create reload function

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import { createSection, loadRandomGrid } from './js/components.mjs';
 const dictionary = apiDictionary('dog');
 let randomDoggos = createSection('Random doggos', true);
 const favouriteDoggos = createSection('Favourite doggos');
+const title = document.getElementById('title-name')
+
+title.innerText = 'Doggies'
 
 loadRandomGrid(dictionary.random, randomDoggos);
 loadRandomGrid(dictionary.favourites, favouriteDoggos);
@@ -15,35 +18,4 @@ loadRandomGrid(dictionary.favourites, favouriteDoggos);
 
 // TODO: create section and add reload button only if does reload(false by default)
 
-/* async function loadDogRandomGrid(ApiURL, sectionID) {
-    const APIRandom = ApiURL;
-    const data = await getData(APIRandom);
-
-    if (typeof data == 'string') {
-        createSpanError(data, sectionID);
-        return;
-    }
-
-    Object.entries(data).forEach(thumbnail => {
-        const [imageURL, imageID] = [thumbnail[1].url, thumbnail[1].id];
-        console.log(thumbnail);
-        createCard(imageURL, sectionID,'save', imageID);
-    });
-} */
-
-/* async function loadDogFavsGrid(behaviour, sectionID) {
-    const APIRandom = urlBuilder(behaviour);
-    const data = await getData(APIRandom);
-
-    if (typeof data == 'string') {
-        createSpanError(data, sectionID);
-        return;
-    }
-
-    Object.entries(data).forEach(thumbnail => {
-        console.log(thumbnail);
-        const [imageURL, imageID] = [thumbnail[1].image.url, thumbnail[1].id];
-        createCard(imageURL, favouriteDoggos,'unsave', imageID);
-    });
-} */
 
